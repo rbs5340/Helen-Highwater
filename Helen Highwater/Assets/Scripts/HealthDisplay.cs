e201e7729b76e7b0c67ddc132b78a667dbdac69d
Ryan Schaufelberger (RIT Student) <rbs5340@g.rit.edu>
2025-03-27 13:46:26 -0400

    Health Display Logic

--- Helen Highwater/Assets/Scripts/HealthDisplay.cs
+++ Helen Highwater/Assets/Scripts/HealthDisplay.cs
@@ -1,9 +1,38 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthDisplay : MonoBehaviour
 {
+    #region Singleton
+    public static HealthDisplay Instance;
+
+    // Sets up instance of the Start Manager Singleton
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.Log("Instance already exists");
+            Destroy(Instance);
+        }
+    }
+    #endregion
+
+    public GameObject hImg;
+    public Sprite[] sprites;
+    /*
+    0 = 0 health/dead, likely unneeded sprite
+    1 = 1 health
+    2 = 2 health
+    3 = 3 health (max)
+    4 = infinite health (mech)
+    */
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +43,17 @@ public class HealthDisplay : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void healthChange(int health)
+    {
+        if (sprites.Length >= health)
+        {
+            hImg.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = sprites[health];
+        }
+        else
+        {
+            Debug.Log("Health Sprite Broke");
+        }
     }
 }
