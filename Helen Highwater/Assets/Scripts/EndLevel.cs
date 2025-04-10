8163f14dc4a995f9414897a8b94975de7fd45a4c
djkas-rit <djk2529@rit.edu>
2025-04-10 01:50:28 -0400

    Implement level end goal

---
+++ Helen Highwater/Assets/Scripts/EndLevel.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    public PauseMenu pauseMenu; // reference to the PauseMenu script
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // check if player enters the trigger in the mech
+        if (other.CompareTag("Player - Mech"))
+        {
+            Debug.Log("End Goal Triggered");
+
+            if (pauseMenu != null)
+            {
+                pauseMenu.EndScreen(); // call the EndScreen method from PauseMenu
+            }
+        }
+    }
+}
